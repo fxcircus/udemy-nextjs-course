2e2a2108ebbf8285d14b6c6468548d2e304e07b2
Roy Daniel <roydanielr@gmail.com>
2023-03-20 06:49:13 -0400

    search form

--- 02_project_file_based_routing/src/pages/events/index.js
+++ 02_project_file_based_routing/src/pages/events/index.js
@@ -1,11 +1,13 @@
 import { getAllEvents } from "../../../dummy-data"
 import EventList from "roy/components/events/EventList"
+import EventsSearch from '../../components/events/EventsSearch'
 
 export default function EventsMainPage() {
 
     const events = getAllEvents()
     return (
       <div>
+        <EventsSearch />
         <EventList items={events} />
       </div>
     )
