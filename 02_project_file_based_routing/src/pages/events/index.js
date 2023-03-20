c88f3f2a25ce1f438f3f0f7e9828269f50d7c37b
Roy Daniel <roydanielr@gmail.com>
2023-03-20 06:59:42 -0400

    filter routing

--- 02_project_file_based_routing/src/pages/events/index.js
+++ 02_project_file_based_routing/src/pages/events/index.js
@@ -1,13 +1,20 @@
 import { getAllEvents } from "../../../dummy-data"
 import EventList from "roy/components/events/EventList"
 import EventsSearch from '../../components/events/EventsSearch'
+import { useRouter } from "next/router"
 
 export default function EventsMainPage() {
-
     const events = getAllEvents()
+    const router = useRouter()
+    
+    function findEventsHandler(year, month) {
+      const fullPath = `/events/${year}/${month}`
+      router.push(fullPath) // navigate to different page
+    }
+
     return (
       <div>
-        <EventsSearch />
+        <EventsSearch onSearch={findEventsHandler} />
         <EventList items={events} />
       </div>
     )
