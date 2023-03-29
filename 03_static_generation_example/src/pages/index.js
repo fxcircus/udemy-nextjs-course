18c2f5c9ab645cd027c538940fbd515c5b7c819c
Roy Daniel <roydanielr@gmail.com>
2023-03-29 07:22:13 -0400

    error handling

--- 03_static_generation_example/src/pages/index.js
+++ 03_static_generation_example/src/pages/index.js
@@ -18,9 +18,23 @@ export async function getStaticProps() {
   const jsonData = await fs.readFile(filePath)
   const data = JSON.parse(jsonData)
 
+  // redirect if no data was fetched from BE
+  if(!data) {
+    return {
+      redirect: {
+        destination: '/no-data'
+      }
+    }
+  }
+
+  // return 404 if no products found
+  if (data.products.length === 0) {
+    return { notFound: true }
+  }
+
   return { props: {
     products: data.products
     },
-    revalidate: 120 // Incremental static generation every 120 seconds at most
+    revalidate: 120 // ISR every 120 seconds at most
   }
 }
